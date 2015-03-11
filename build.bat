1a512e0a92fa97f4111dba8d2a6c345914df1e02
Sébastien Rombauts <sebastien.rombauts@gmail.com>
2015-03-11 13:57:14 +0100

    Fix comments

--- build.bat
+++ build.bat
@@ -1,10 +1,17 @@
-@REM Copyright (c) 2013 Sebastien Rombauts (sebastien.rombauts@gmail.com)
+@REM Copyright (c) 2013-2015 Sebastien Rombauts (sebastien.rombauts@gmail.com)
 @REM
 @REM Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
 @REM or copy at http://opensource.org/licenses/MIT)
 mkdir build
 cd build
+
+# Generate a Visual Studio solution for latest version found
 cmake .. @REM -G "Visual Studio 10"
+
+# Build default configuration (ie 'Debug')
 cmake --build .
+
+# Run unit-tests (ie 'make test')
 ctest .
+
 cd ..
