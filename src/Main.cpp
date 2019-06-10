459e5cc08a637d9c6daaaceaa25f2a609379925a
Sébastien Rombauts <sebastien.rombauts@gmail.com>
2019-06-10 22:11:03 +0200

    Complete Bootstrap example with a proper main div container

--- src/Main.cpp
+++ src/Main.cpp
@@ -23,12 +23,18 @@
 int main() {
     HTML::Document document("Welcome to HTML");
     document.addAttribute("lang", "en");
+
+    // Head
     document.head() << HTML::Meta("utf-8")
         << HTML::Meta("viewport", "width=device-width, initial-scale=1, shrink-to-fit=no");
     document.head() << HTML::Rel("stylesheet", "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css")
         .integrity("sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T").crossorigin("anonymous");
     document.head() << HTML::Style(".navbar{margin-bottom:20px;}");
 
+    // Body
+    document.body().cls("bg-light");
+
+    // Navbar
     HTML::List navList(false, "navbar-nav mr-auto");
     navList << std::move(HTML::ListItem().cls("nav-item active") << HTML::Link("Home", "#").cls("nav-link"));
     navList << std::move(HTML::ListItem().cls("nav-item") << HTML::Link("Link", "#").cls("nav-link"));
@@ -42,31 +48,36 @@ int main() {
     );
     document << (HTML::Nav("navbar navbar-expand navbar-dark bg-dark") << (HTML::Div("collapse navbar-collapse") << std::move(navList)));
 
-    document << HTML::Header1("Welcome to HTML").id("anchor_link_1");
-    document.body() << "Text directly in the body. ";
-    document << HTML::Text("Text directly in the body. ") << HTML::Text("Text directly in the body.") << HTML::Break()
+    // Content in a container
+    HTML::Div main("container");
+    main << HTML::Header1("Welcome to HTML").id("anchor_link_1");
+    main << "Text directly in the body.";
+    main << HTML::Text("Text directly in the body. ") << HTML::Text("Text directly in the body.") << HTML::Break()
         << HTML::Text("Text directly in the body.");
-    document << HTML::Paragraph("This is the way to go for a big text in a multi-line paragraph.");
-    document << HTML::Link("Google", "http://google.com").cls("my_style");
-    document << (HTML::Paragraph("A paragraph. ").style("font-family:arial")
+    main << HTML::Paragraph("This is the way to go for a big text in a multi-line paragraph.");
+    main << HTML::Link("Google", "http://google.com").cls("my_style");
+    main << (HTML::Paragraph("A paragraph. ").style("font-family:arial")
         << HTML::Text("Text child.") << HTML::Break() << HTML::Text("And more text."));
 
-    document << (HTML::List()
+    main << (HTML::List()
         << (HTML::ListItem("Text item"))
         << (HTML::ListItem() << HTML::Link("Github Link", "http://srombauts.github.io").title("SRombaut's Github home page"))
         << (HTML::ListItem() << (HTML::List()
                 << HTML::ListItem("val1")
                 << HTML::ListItem("val2"))));
 
-    document << (HTML::Table()
+    main << (HTML::Table().cls("table table-hover table-sm")
         << HTML::Caption("Table caption")
         << (HTML::Row() << HTML::ColHeader("A") << HTML::ColHeader("B"))
         << (HTML::Row() << HTML::Col("Cell_11") << HTML::Col("Cell_12"))
         << (HTML::Row() << HTML::Col("Cell_21") << HTML::Col("Cell_22"))
         << (HTML::Row() << HTML::Col("") << HTML::Col("Cell_32")));
 
-    document << HTML::Link().id("anchor_link_2");
+    main << HTML::Link().id("anchor_link_2");
 
+    document << std::move(main);
+
+    // Javascript at the end of the body
     document << HTML::Script("https://code.jquery.com/jquery-3.3.1.slim.min.js")
         .integrity("sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo").crossorigin("anonymous");
     document << HTML::Script("https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js")
